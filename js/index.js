202b1798c5a087bade4c034bb02f9afef55f7435
ytxmobile98 <5900105+ytxmobile98@users.noreply.github.com>
2023-09-28 01:01:18 +0800

    feat: load demo text

---
+++ js/index.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const root = document.getElementById('root');
+const domParser = new DOMParser();
+
+const fontWeights = {
+    100: 'Thin',
+    200: 'Extra Light',
+    300: 'Light',
+    400: 'Regular',
+    500: 'Medium',
+    600: 'Semi Bold',
+    700: 'Bold',
+    800: 'Extra Bold',
+    900: 'Black',
+};
+
+const demoText = {
+    English: 'The quick brown fox jumps over the lazy dog.',
+    Chinese: '敏捷的棕色狐狸跃过懒狗。',
+};
+
+const fontFamiliesByGroup = [
+    [
+        'sans-serif',
+        ['sans-serif']
+    ],
+    [
+        'Noto Sans + sans-serif',
+        ['Noto Sans', 'sans-serif']
+    ],
+    [
+        'Noto Sans + Noto Sans CJK SC + sans-serif',
+        ['Noto Sans', 'Noto Sans CJK SC', 'sans-serif']
+    ],
+]
+
+/**
+ * Render single block, with font weights 100~900.
+ * @param {string} header header text.
+ * @param {string[]} fontFamilies font families.
+ */
+function renderSingleBlock(header, fontFamilies) {
+    const blockText =
+        `<div class="block" style="font-family: ${fontFamilies.join(', ')}">
+            <h1 class="block__header">${header}</h1>
+            ${Object.entries(fontWeights)
+                .map(([weight, name]) =>
+                    `<div class="block__single-demo">
+                        <p class="block__font-weight-name">${weight} ${name}</p>
+                        <p class="block__demo-text"
+                            style="font-weight: ${weight}">${demoText.English} ${demoText.Chinese}</p>
+                    </div>`)
+                .join('')}
+         </div>`;
+
+    const body = domParser.parseFromString(blockText, 'text/html').body;
+    return body;
+}
+
+/**
+ * Render all blocks.
+ * @param {HTMLElement} root root element.
+ */
+function renderAllBlocks(root) {
+    const blocks = fontFamiliesByGroup.map(
+        ([header, fontFamilies]) => renderSingleBlock(header, fontFamilies)
+    );
+
+    root.append(...blocks);
+}
+
+document.addEventListener('DOMContentLoaded', () => renderAllBlocks(root));
